76061670700a1e82d25c4e4d3870de03c229e181
Dan Reilly <danreil@gmail.com>
2023-08-07 12:07:08 -0500

    Update Euler22.py

--- Euler22.py
+++ Euler22.py
@@ -51,13 +51,6 @@ def convert_hand(hand_str):
     hand_record_sort = np.sort(hand_record, order = 'ranks')
     return hand_record_sort
 
-def detect_best_hand(hand_array):
-    """
-    Detect the type of hand that the input 5 cards has from the deal (ie, pair, flush, etc.)
-    :param hand_array: output of convert_hand. A numpy record array
-    :return: TBD
-    """
-    pass
 
 def detect_pairs(hand_array):
     # Detect any of the types of pairs in the hand (1 pair, 2 pair, 3 of kind, 4 of a kind)
@@ -82,33 +75,82 @@ def detect_pairs(hand_array):
     else:
         result_hand = hand_order['High Card']
     
-    return result_hand.name        
-        
+    return result_hand         
 
 
 def detect_straight(hand_array):
-    # As an example, might change. But try detecting a straight, and return highest number in straight
+    """ Check whether hand_array has a straight. If so, return true."""
     ranks = hand_array.ranks
     result = True
     for i in range(num_cards-1):
         if ranks[i] != ranks[i+1] - 1:
             result = False
     # Note, haven't implemented the option of ace being low. Not sure if it should or not
-    return result, ranks[num_cards-1]
+    return result
 
-#def detect_flush    
+def detect_flush(hand_array):
+    """ Check whether hand_array has a Flush. If so, return true."""
+    suits = hand_array.suits
+    result = False
+    if len(np.unique(suits)) == 1:
+        result = True
+    
+    return result   
 
+def find_hand_value(hand_array):
+    """ Call all of the detect_XX functions to find the value of a player's hand"""
+    value_pair = detect_pairs(hand_array) # this func directly returns the hand value
+    value_nonpair = 0
+    is_straight = detect_straight(hand_array)
+    is_flush = detect_flush(hand_array)
+    is_straight_flush = is_straight and is_flush
+    if is_straight:
+        value_nonpair = hand_order['Straight']
+    if is_flush:
+        value_nonpair = hand_order['Flush']
+    if is_straight_flush:
+        value_nonpair = hand_order['Straight Flush']
+    
+    # The greater of value_nonpair and value_pair is the hand_value
+    value = max(value_pair, value_nonpair)
 
-# hand_test = player2[14]
-# print(hand_test)
-# convert_hand_test = convert_hand(hand_test)
-# print(convert_hand_test.ranks)
-# print(convert_hand_test.suits)
-# is_straight, high_card = detect_straight(convert_hand_test)
-# print(convert_hand_test)
+    return value
+"""
+def compare_tie(player1_hand, player2_hand, player1_value):
+    """ In case of tie, follow this complicated logic to resolve it"""
+    if player1_value == 1: # High Card
+        if hand1_arr.ranks > hand2_arr.ranks:
+            player_result = [1, 0]
+        else:
+            player_result = [0, 1]
+        
+    return player_result
+"""
 
-hand_test_manual = np.array(['4H','4J','7H','5H','5H'])
-convert_hand_test_manual = convert_hand(hand_test_manual)
-# is_straight, high_card = detect_straight(convert_hand_test_manual)
-result_hand = detect_pairs(convert_hand_test_manual)
-print(result_hand)
+def detect_best_hand(player1_hand, player2_hand):
+    """
+    Find out the best hand type for each player, and find out which is best
+    Input: Hand arrays (converted, as returned from convert hand) for player 1 and 2
+    Return: A numpy array of [1,0] if player 1 wins, or [0,1] if player 2 wins 
+    """
+    player1_value = find_hand_value(player1_hand)
+    player2_value = find_hand_value(player2_hand)
+    
+    if player1_value > player2_value:
+        player_result = [1, 0]
+    elif player2_value > player1_value:
+        player_result = [0, 1]
+    else:
+        player_result = compare_tie(player1_hand, player2_hand, player1_value)
+    
+    return player_result
+
+def card_strength_calc(hand_array):
+    """ New Idea, assign values to card ranks, such that it correctly compares
+    high cards all the way down (like, 14^5+14^3...)"""
+
+hand1 = np.array(['10H','6C', '5H', '4S', '3D'])
+hand2 = np.array(['JH','7C', '6S', '5S', '4D'])
+hand1_arr, hand2_arr = convert_hand(hand1), convert_hand(hand2)
+
+best_hand = detect_best_hand(hand1_arr, hand2_arr)
